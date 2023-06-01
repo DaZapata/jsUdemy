16d63809413694e04cab12d22b9e7e1ebe42e9a9
David Zapata <cristian.carrillo@controlt.com.co>
2023-05-31 21:48:36 -0500

    funciones

--- 04-Funciones/app.js
+++ 04-Funciones/app.js
@@ -6,19 +6,14 @@ bienvenido();
 //partes de una funcion
 //parametros, codigo, salida
 
-// function cuadraro(num) {
-//     var resultado = num * num;
-//     return resultado;
-// }
-
-const resul = (num) =>{
-    var respo = num*num
-    return respo
+function cuadraro(num) {
+    var resultado = num * num;
+    return resultado;
 }
 
 var num = 3;
-var res = resul(num);
-console.warn(res);
+var res = cuadraro(num);
+console.log(res);
 
 const calcularEdad = (anioNacimiento) => {
     var anoActual = 2023 - anioNacimiento;
@@ -29,3 +24,34 @@ var edadJubilacion = 65;
 var teFaltan = 65 - calcularEdad(1987);
 
 console.log(`te faltan ${teFaltan} anios para jubilarte`);
+
+//************************* */
+//funciones como expresiones
+
+var prueba = function(){
+    return 'mensaje de prueba'
+}
+console.log(prueba());
+console.log('***************');
+/*parametros indefinidos */
+var name;
+var test = function(n) {
+    return 'hola ' + n;
+}
+console.log(test(name));
+console.log('***************');
+
+/**argumentos nullos */
+var a;
+a = null;
+var valorNulo = function(){
+    return a;
+}
+console.log(valorNulo());
+console.log('***************');
+
+/**argumentos por defecto */
+var sumar = function(a, b, c = 4){
+    return a + b + c;
+}
+console.log(sumar(10, 4));
